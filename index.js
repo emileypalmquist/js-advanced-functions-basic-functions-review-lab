3347ee819a10cc7b2dda78d6035ea9c13cbfeeaf
github username <github email address>
2019-11-07 14:49:33 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adj='*') {
+  return function(adjective='special') {
+    return `You are ${adj}${adjective}${adj}!`
+  }
+}
+
+const Calculator = {
+    add: function(a, b) { return a + b },
+    subtract: function(a,b) { return a - b },
+    multiply: function(a,b) { return a * b },
+    divide: function(a,b) { return a / b }
+}
+
+function actionApplyer(startInt, arrayOfFunctions) {
+  if (arrayOfFunctions.length === 0) {
+    return startInt
+  } else {
+    (startInt * 2 + 1000) % 7
+    return 4
+  }
+}
